7675b15f943cefc93dcc70c8b10a727d17a3411c
Thomas-L-Dev <loicb.thomas@gmail.com>
2021-10-28 09:47:50 +0200

    💄 🎨 add layoutDefault.vue

--- web_interface/src/main.js
+++ web_interface/src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 
-createApp(App).use(store).use(router).mount("#app");
+library.add(fas);
+
+createApp(App)
+  .component("fa", FontAwesomeIcon)
+  .use(ElementPlus)
+  .use(store)
+  .use(router)
+  .mount("#app");
