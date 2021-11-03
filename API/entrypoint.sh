62110c4d976708d1fc026c28b165f69d96db188b
Loic-Thomas-MscPro <loic1.thomas@epitech.eu>
2021-11-03 21:44:51 +0100

    ⚡️

--- API/entrypoint.sh
+++ API/entrypoint.sh
@@ -4,23 +4,21 @@
 ls
 
 # Wait until Postgres is ready
-echo "Testing if Postgres is accepting connections. {$PGHOST} {$PGPORT} ${PGUSER}"
-while ! pg_isready -q -h $PGHOST -p $PGPORT -U $PGUSER
+echo "Testing if Postgres is accepting connections. {db} {5432} {postgres}"
+while ! pg_isready -q -h db -p 5432 -U postgres
 do
   echo "$(date) - waiting for database to start"
   sleep 2
 done
 
 # Create, migrate, and seed database if it doesn't exist.
-if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
-  echo "Database $PGDATABASE does not exist. Creating..."
+if [[ -z `psql -Atqc "\\list time_manager_dev"` ]]; then
+  echo "Database time_manager_dev does not exist. Creating..."
   
   mix ecto.create
   mix ecto.migrate
   # mix run priv/repo/seeds/user_seeds.exs
   # mix run priv/repo/seeds/clock_seeds.exs
   # mix run priv/repo/seeds/workingtime_seeds.exs
-  echo "Database $PGDATABASE created."
+  echo "Database time_manager_dev created."
 fi
-
-# exec mix phx.server
